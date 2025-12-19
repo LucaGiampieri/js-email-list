3bcaca75db8d63cdb169867197b7c42f271277bc
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-19 15:47:11 +0100

    Aggiunta bottone per generare nuove email

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,24 @@
 // selezione degli elementi del DOM
 
 const emailList = document.getElementById("emailList");
-
+const button = document.getElementById("button");
 const emailGenerator = "https://flynn.boolean.careers/exercises/api/random/mail";
 
+//avvio genrazione
+emailGeneratorFunction();
 
+button.addEventListener("click", emailGeneratorFunction);
+
+function emailGeneratorFunction (){
 let emailOutput = '';
-
 for (let i = 0; i < 10; i++) {
     axios.get(emailGenerator)
         .then(response => {
-            emailOutput += `<li>${response.data.response}</li>`;
+            emailOutput += `<li class="emailSingle">${response.data.response}</li>`;
             emailList.innerHTML = emailOutput;
         })
         .catch(error => {
             console.error(error.message);
         });
+}
 }
